e766711908913e25bf152ec77dba9ce06f1cdf12
kwicen <kwicento@ualberta.ca>
2014-01-16 15:14:29 -0700

    made new important tweet class

---
+++ src/ca/ualberta/cs/lonelytwitter/ImportantTweetModel.java
@@ -0,0 +1,26 @@
+package ca.ualberta.cs.lonelytwitter;
+
+import java.util.Date;
+
+
+public class ImportantTweetModel extends LonelyTweetModel
+{
+
+	public ImportantTweetModel(String text, Date timestamp)
+	{
+
+		super(text, timestamp);
+		// TODO Auto-generated constructor stub
+	}
+
+	public ImportantTweetModel(String text)
+	{
+
+		super(text);
+		// TODO Auto-generated constructor stub
+	}
+	
+	public String getText() {
+		return "Important! " + super.getText();
+	}
+}
